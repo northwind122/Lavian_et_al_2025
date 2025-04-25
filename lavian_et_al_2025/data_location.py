d80e48a8ac97c9a35ea7afaca56afc5ebd17add8
hlavian <hagar.lavian@gmail.com>
2025-04-25 11:41:59 +0200

    Importing data paths from main repository

---
+++ lavian_et_al_2025/data_location.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+
+master_all = Path("Z:\Hagar\main\lavian_et_al_2025")
+
+master_motion = master_all / "visual motion"
+master_landmarks = master_all / "landmarks"
+master_hd = master_all / "heading direction after habenula ablation"
