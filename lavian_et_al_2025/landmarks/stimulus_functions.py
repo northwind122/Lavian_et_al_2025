010298f4f3babbdb8b12eab54c1f607fee817467
hlavian <hagar.lavian@gmail.com>
2025-05-26 11:48:47 +0200

    removing old unnecessary functions

--- lavian_et_al_2025/landmarks/stimulus_functions.py
+++ lavian_et_al_2025/landmarks/stimulus_functions.py
@@ -3,7 +3,7 @@ import pandas as pd
 from scipy.interpolate import interp1d
 from scipy.signal import convolve2d
 from lavian_et_al_2025.visual_motion.colors import JCh_to_RGB255
-from lavian_et_al_2025.visual_motion.stimulus_functions import stim_vel_dir_dataframe, quantize_directions
+from lavian_et_al_2025.visual_motion.stimulus_functions import quantize_directions
 
 
 HUESHIFT = 2.5
@@ -92,55 +92,6 @@ def get_tuning_map_pixels(img, sens_regs, n_dirs=8):
     amp = np.sqrt(np.sum(reg_vectors ** 2, 0))
 
     return amp, angle
-
-
-def make_sensory_regressors(exp, n_dirs=8, upsampling=5, sampling=1 / 3):
-    """
-    Creates landmark position regressors from experiment data.
-
-    This function creates indicator variables for each position, upsamples them, applies an exponential decay kernel,
-    and then downsamples back to the original rate. The resulting regressors model the expected neural response to
-    landmarks at different positions.
-
-    Parameters
-    ----------
-    exp : object
-        Experiment object containing data with landmark positions
-    n_dirs : int, default=8
-        Number of position bins to use (evenly spaced around 360 degrees). Never actually used, stimulus was always
-        split into 8 positions.
-    upsampling : int, default=5
-        Factor by which to upsample the data for smoother convolution
-    sampling : float, default=1/3
-        Imaging sampling rate in Hz
-
-    Returns
-    -------
-    pd.DataFrame
-        DataFrame with columns for each position regressor
-    """
-    stim = stim_vel_dir_dataframe(exp)
-    bin_centres, dir_bins = quantize_directions(stim.theta)
-    ind_regs = np.zeros((n_dirs, len(stim)))
-    for i_dir in range(n_dirs):
-        ind_regs[i_dir, :] = (np.abs(dir_bins - i_dir) < 0.1) & (stim.vel > 0.1)
-
-    dt_upsampled = sampling / upsampling
-    t_imaging_up = np.arange(0, stim.t.values[-1], dt_upsampled)
-    reg_up = interp1d(stim.t.values, ind_regs, axis=1, fill_value="extrapolate")(
-        t_imaging_up
-    )
-
-    # 6s kernel
-    u_steps = t_imaging_up.shape[0]
-    u_time = np.arange(u_steps) * dt_upsampled
-    decay = np.exp(-u_time / (1.5 / np.log(2)))
-    kernel = decay / np.sum(decay)
-
-    convolved = convolve2d(reg_up, kernel[None, :])[:, 0:u_steps]
-    reg_sensory = convolved[:, ::upsampling]
-
-    return pd.DataFrame(reg_sensory.T, columns=[f"motion_{i}" for i in range(n_dirs)])
 
 
 def color_stack(
